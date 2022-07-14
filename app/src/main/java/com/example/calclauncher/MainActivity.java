9577a460fd4b0a107a006e55cd1f2b03b43f0914
ivanblkn <workbinom@mail.ru>
2022-07-14 16:33:06 +0400

    final

---
+++ app/src/main/java/com/example/calclauncher/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.calclauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_Launch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calcIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("calculator://launch"));
+                startActivity(Intent.createChooser(calcIntent, null));
+            }
+        });
+    }
+}
